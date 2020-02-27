d070bdf756f12c9632076994ab25dc1139f584a7
RiemaruKarurosu <18121504@tecmor.mx>
2020-02-27 11:52:28 -0600

    version1.0

--- conexion.py
+++ conexion.py
@@ -1,21 +1,3 @@
 import os
-red= "200.33.171.0"
-hostname = red
-
-respuesta = os.system("ping -c 1 " + hostname)
-
-if respuesta ==0:
-    print (hostname + ": esta funcionamiento!")
-else:
-    print (hostname + ": NO funciona!")
-
-#deteccion de computadoras
-
-os.system("nmap -sP "+red)
-
-#deteccion de puertos abiertos
-computadora=red
-os.system("nmap -sT "+computadora)
-
-#Detectar sistema operativo
-os.system("nmap -O "+computadora)
+red= "200.33.171.0/24"
+os.system("nmap -sP "+ red)
