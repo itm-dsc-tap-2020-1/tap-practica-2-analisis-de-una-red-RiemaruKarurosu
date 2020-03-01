f832a685065622a6fd9517a508e6c76b766e1fa5
RiemaruKarurosu <18121504@tecmor.mx>
2020-03-01 07:06:16 -0600

    v.02

--- conexion.py
+++ conexion.py
@@ -2,4 +2,39 @@ import os
 red= "200.33.171.0/24"
 os.system("nmap -sP "+ red)
 
-""
+"""
+Parallel DNS resolution of 256 hosts. Timing: About 50.00% done; ETC: 11:57 (0:00:06 remaining)
+Nmap scan report for delfin2.itmorelia.edu.mx (200.33.171.11)
+Host is up (0.0010s latency).
+Nmap scan report for 200.33.171.13
+Host is up (0.0014s latency).
+Nmap scan report for dsc.itmorelia.edu.mx (200.33.171.20)
+Host is up (0.0010s latency).
+Nmap scan report for libra.itmorelia.edu.mx (200.33.171.54)
+Host is up (0.00041s latency).
+Nmap scan report for 200.33.171.66
+Host is up (0.0011s latency).
+Nmap scan report for denebvirtual.itmorelia.edu.mx (200.33.171.77)
+Host is up (0.00088s latency).
+Nmap scan report for sappp.itmorelia.edu.mx (200.33.171.80)
+Host is up (0.0011s latency).
+Nmap scan report for 200.33.171.86
+Host is up (0.0010s latency).
+Nmap scan report for 200.33.171.99
+Host is up (0.0011s latency).
+Nmap scan report for 200.33.171.115
+Host is up (0.0012s latency).
+Nmap scan report for 200.33.171.120
+Host is up (0.00093s latency).
+Nmap scan report for vinculacion.itmorelia.edu.mx (200.33.171.124)
+Host is up (0.0011s latency).
+Nmap scan report for 200.33.171.125
+Host is up (0.00094s latency).
+Nmap scan report for 200.33.171.127
+Host is up (0.00066s latency).
+Nmap scan report for 200.33.171.129
+Host is up (2.9s latency).
+Nmap scan report for 200.33.171.254
+Host is up (0.0013s latency).
+Nmap done: 256 IP addresses (16 hosts up) scanned in 195.77 seconds
+"""
